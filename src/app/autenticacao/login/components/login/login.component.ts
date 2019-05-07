bd0902d1149ccc06669c99161aa75ef6e39739e4
prmorais13 <prmorais_13@hotmail.com>
2019-05-07 17:12:04 -0300

    13. Criando a tela de login

--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit() {
+    this.gerarForm();
+  }
+
+  gerarForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  logar() {
+    if (this.form.invalid) {
+      this.snackBar.open(
+        'Dados inválidos', 'Error', {duration: 5000}
+      );
+      return;
+    }
+    alert(JSON.stringify(this.form.value));
   }
 
 }
--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { LoginComponent } from './components/login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { LogarComponent } from './components/logar.component';
+import {
+  MatInputModule, MatButtonModule,
+  MatListModule, MatTooltipModule,
+  MatIconModule, MatSnackBarModule
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [LoginComponent, LogarComponent],
   imports: [
     CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule,
+
     LoginRoutingModule
   ]
 })
