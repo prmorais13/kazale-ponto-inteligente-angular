c93b6ec9036aeb1f6bfe0e47f145d462de8f37be
prmorais13 <prmorais_13@hotmail.com>
2019-05-08 16:40:40 -0300

    Criando o formulario de pessoa juridica

--- src/app/autenticacao/cadastro-pj/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/cadastrar-pj/cadastrar-pj.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-cadastrar-pj',
   templateUrl: './cadastrar-pj.component.html',
-  styleUrls: ['./cadastrar-pj.component.scss']
+  styleUrls: ['./cadastrar-pj.component.scss'],
+  preserveWhitespaces: true
 })
 export class CadastrarPjComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit() {
+    this.gerarForm();
+  }
+
+  gerarForm() {
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      cpf: ['', [Validators.required]],
+      razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
+      cnpj: ['', [Validators.required]]
+    });
+  }
+
+  cadastrarPj() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    alert(JSON.stringify(this.form.value));
   }
 
 }
--- src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatListModule,
+  MatTooltipModule,
+  MatIconModule,
+  MatSnackBarModule
+} from '@angular/material';
 
 import { CadastroPjRoutingModule } from './cadastro-pj-routing.module';
 import { CadastrarPjComponent } from './cadastrar-pj/cadastrar-pj.component';
 import { CadastroPjComponent } from './cadastro-pj.component';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 @NgModule({
   declarations: [CadastrarPjComponent, CadastroPjComponent],
   imports: [
     CommonModule,
-    CadastroPjRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
     FlexLayoutModule,
-    RouterModule
+    CadastroPjRoutingModule
   ]
 })
-export class CadastroPjModule { }
+export class CadastroPjModule {}
