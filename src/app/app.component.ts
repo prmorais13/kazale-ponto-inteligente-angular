577038dd117984d1f5ed1af5362eb8f2096b29cd
prmorais13 <prmorais_13@hotmail.com>
2019-05-10 15:42:23 -0300

    Adicionando o link de sair (Logout)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ponto-inteligente';
+
+  constructor(private router: Router) {}
+
+  sair() {
+    delete localStorage.token;
+    this.router.navigate(['/']);
+  }
+
+  autenticado(): boolean {
+    return localStorage.token;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule } from '@angular/material';
 
-import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { LoginModule } from './autenticacao/login/login.module';
 import { AppRoutingModule } from './app-routing.module';
 import { CadastroPjModule } from './autenticacao/cadastro-pj/cadastro-pj.module';
 import { CadastroPfModule } from './autenticacao/cadastro-pf/cadastro-pf.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
     LoginModule,
     CadastroPjModule,
     CadastroPfModule,
